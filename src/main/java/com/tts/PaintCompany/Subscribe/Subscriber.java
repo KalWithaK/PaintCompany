e42abdcf7dd1f27b1c93d7614804dbb9461bf757
KalWithaK <schumacher.kal24@gmail.com>
2019-05-20 12:13:57 -0400

    Revert "merge to master"
    This reverts commit fa54bc99883c4b4e17f0704569326c55ca849609.

--- src/main/java/com/tts/PaintCompany/Subscribe/Subscriber.java
+++ src/main/java/com/tts/PaintCompany/Subscribe/Subscriber.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
@@ -22,10 +24,21 @@ public class Subscriber {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
 	
+//	@Getter
+//	@Setter
+//	@NotEmpty(message = "Please enter your first name")
+//	private String firstName;
+//	
+//	@Getter
+//	@Setter
+//	@NotEmpty(message = "Please enter your last name")
+//	private String lastName;
+	
 	@Getter
 	@Setter
-	@NotEmpty(message = "Please enter your email address")
-	@Email(message = "Please enter a valid email address")
+	//@NotEmpty(message = "Please enter your email address")
+	//@Email(message = "Please enter a valid email address")
+	// @Column(unique = true)
 	private String email;
 		
 	@Column
@@ -37,14 +50,28 @@ public class Subscriber {
 	public Subscriber() {
 	}
 	
+//	public Subscriber(String firstName, String lastName, String email, Date signedUp) {
+//		this.firstName = firstName;
+//		this.lastName = lastName;
+//		this.email = email;
+//		this.signedUp = signedUp;
+//	  }
+	
 	public Subscriber(String email, Date signedUp) {
 		this.email = email;
 		this.signedUp = signedUp;
 	  }
 	
+//	@Override
+//	public String toString() {
+//		return "Subscriber [id= " + id + ", firstName= " + firstName + ", lastName= " + lastName + ", email= " + email
+//	+ ", signedUp= " + signedUp + "]";
+//	}
+	
 	@Override
 	public String toString() {
-		return "Subscriber [id= " + id  +  ", email= " + email + ", signedUp= " + signedUp + "]";
+		return "Subscriber [id= " + id  +  ", email= " + email
+	+ ", signedUp= " + signedUp + "]";
 	}
 
 }
--- src/main/java/com/tts/PaintCompany/Subscribe/SubscriberRepository.java
+++ src/main/java/com/tts/PaintCompany/Subscribe/SubscriberRepository.java
@@ -3,5 +3,6 @@ package com.tts.PaintCompany.Subscribe;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SubscriberRepository extends CrudRepository<Subscriber, Long>{
+	//Subscriber findByEmail(String email);
 	
 }
